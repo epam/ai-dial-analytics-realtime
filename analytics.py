d31aba45c51987453a096e71e03ce9fd99520f51
Vladislav Vishnevsky <vvishnevsky@deltixlab.com>
2023-08-11 12:56:23 +0200

    add interface for analytics

---
+++ analytics.py
@@ -0,0 +1,37 @@
+from influxdb_client import Point, WritePrecision
+from datetime import datetime
+import os
+from logging import Logger
+from influxdb_client.client.influxdb_client_async import InfluxDBClientAsync
+
+influx_org = os.environ.get('INFLUX_ORG')
+influx_bucket = os.environ.get('INFLUX_BUCKET')
+
+async def on_message(logger: Logger,
+                     influx_write_api: InfluxDBClientAsync,
+                     deployment: str,
+                     model: str,
+                     project_id: str,
+                     chat_id: str,
+                     request: any,
+                     response: any):
+    usage = response['usage']
+
+    logger.info(f'Chat completion response length {len(response)}')
+
+    point = Point('analytics') \
+        .tag('model', model) \
+        .tag('deployment', deployment) \
+        .tag('model', model) \
+        .tag('project_id', project_id) \
+        .field('number_request_message', len(request['messages'])) \
+        .time(datetime.utcnow(), WritePrecision.NS)
+
+    if len(chat_id) > 0:
+        point.tag('chat_id', chat_id)
+
+    if usage != None:
+        point.field('completion_tokens', usage['completion_tokens']) \
+            .field('prompt_tokens', usage['prompt_tokens'])
+
+    await influx_write_api.write(influx_bucket, influx_org, point)
--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import os
 import json
 import re
 from influxdb_client.client.influxdb_client_async import InfluxDBClientAsync
-from influxdb_client.client.write_api import ASYNCHRONOUS
+from analytics import on_message
+from universal_api_utils import merge
 
 RATE_PATTERN = r"/v1/rate"
 CHAT_COMPLETION_PATTERN = r"/openai/deployments/(.+?)/chat/completions"
@@ -13,7 +14,6 @@ CHAT_COMPLETION_PATTERN = r"/openai/deployments/(.+?)/chat/completions"
 influx_url = os.environ.get('INFLUX_URL')
 influx_api_token = os.environ.get('INFLUX_API_TOKEN')
 influx_org = os.environ.get('INFLUX_ORG')
-influx_bucket = os.environ.get('INFLUX_BUCKET')
 
 app = FastAPI()
 logging.basicConfig(
@@ -27,10 +27,10 @@ logger.setLevel(logging.DEBUG)
 
 @app.on_event("startup")
 async def startup_event():
-    global client, write_api
+    global client, influx_write_api
     
     client = InfluxDBClientAsync(url=influx_url, token=influx_api_token, org=influx_org)
-    write_api = client.write_api()
+    influx_write_api = client.write_api()
 
 @app.on_event("shutdown")
 async def shutdown_event():
@@ -38,16 +38,6 @@ async def shutdown_event():
 
 async def on_rate_message(request, response):
     logger.info(f'Rate message length {len(request) + len(response)}')
-
-async def publish_usage(deployment, model, project_id, chat_id, usage):
-    logger.info(f'Chat completion message length {usage["total_tokens"]}')
-
-    data = [
-        f"tokens,deployment={deployment},model={model},project_id={project_id} completion_tokens={usage['completion_tokens']}",
-        f"tokens,deployment={deployment},model={model},project_id={project_id} prompt_tokens={usage['prompt_tokens']}",
-    ]
-    await write_api.write(influx_bucket, influx_org, data)
-
 
 async def on_chat_completion_message(deployment, project_id, chat_id, request, response):
     if response['status'] != '200':
@@ -57,6 +47,7 @@ async def on_chat_completion_message(deployment, project_id, chat_id, request, r
     stream = request_body.get('stream', False)
     model = request_body.get('model', deployment)
 
+    response_body = None
     if stream:
         body = response['body']
         chunks = body.split('\n\ndata: ')
@@ -64,19 +55,18 @@ async def on_chat_completion_message(deployment, project_id, chat_id, request, r
         chunks[0] = chunks[0].removeprefix('data: ')
         chunks = chunks[0:len(chunks)-1] # remove [DONE] chunk
 
-        for chunk in chunks:
+        response_body = json.loads(chunks[-1])
+        for chunk in chunks[0:len(chunks)-1]:
             chunk = json.loads(chunk)
 
-            if 'usage' in chunk and chunk['usage'] != None:
-                await publish_usage(deployment, model, project_id, chat_id, chunk['usage'])
+            response_body['choices'] = merge(response_body['choices'], chunk['choices'])
     else:
-        body_json = json.loads(response['body'])
+        response_body = json.loads(response['body'])
 
-        if 'usage' in body_json and body_json['usage'] != None:
-            await publish_usage(deployment, model, project_id, chat_id, body_json['usage'])
+    await on_message(logger, influx_write_api, deployment, model, project_id, chat_id, request_body, response_body)
 
 
-async def on_new_message(message):
+async def on_log_message(message):
     request = message['request']
     uri = message['request']['uri']
     response = message['response']
@@ -93,11 +83,11 @@ async def on_new_message(message):
         await on_chat_completion_message(deployment, project_id, chat_id, request, response)
 
 @app.post('/data')
-async def chat(request: Request):
+async def on_log_messages(request: Request):
     data = await request.json()
 
     for item in data:
-        await on_new_message(json.loads(item['message']))
+        await on_log_message(json.loads(item['message']))
 
 @app.get('/health')
 def health():

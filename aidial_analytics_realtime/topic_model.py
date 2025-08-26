de03eb32261ea2e79c28cb69f01f0843b97f9225
Anton Dubovik <anton_dubovik@epam.com>
2025-08-26 14:25:49 +0200

    fix: support disabling topics via empty env var (#156)

--- aidial_analytics_realtime/topic_model.py
+++ aidial_analytics_realtime/topic_model.py
@@ -70,13 +70,13 @@ def create_topic_model(
     topic_model: str | None = None,
     topic_embeddings_model: str | None = None,
 ) -> TopicModel:
-    topic_model = topic_model or os.getenv("TOPIC_MODEL")
+    topic_model = topic_model or os.getenv("TOPIC_MODEL") or None
 
     if topic_model is None:
         return TopicModelNoOp()
 
-    topic_embeddings_model = topic_embeddings_model or os.getenv(
-        "TOPIC_EMBEDDINGS_MODEL"
+    topic_embeddings_model = (
+        topic_embeddings_model or os.getenv("TOPIC_EMBEDDINGS_MODEL") or None
     )
 
     return TopicModelBERT.create(

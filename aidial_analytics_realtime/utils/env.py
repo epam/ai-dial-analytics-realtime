af91700d7ee65722dbb1a2e6e23356f88faa9500
Anton Dubovik <anton_dubovik@epam.com>
2026-02-27 16:00:57 +0000

    fix: simplify code

--- aidial_analytics_realtime/utils/env.py
+++ aidial_analytics_realtime/utils/env.py
@@ -1,8 +1,7 @@
 import os
-from typing import Optional
 
 
-def get_env(name: str, err_msg: Optional[str] = None) -> str:
+def get_env(name: str) -> str:
     if (val := os.getenv(name)) is not None:
         return val
-    raise Exception(err_msg or f"{name} env variable is not set")
+    raise Exception(f"{name} env variable is not set")
